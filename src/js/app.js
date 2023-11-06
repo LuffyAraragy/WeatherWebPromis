96b5c92092c9502ae97a9a018185f1df2c28e46b
Luffy <ponosonik1@mail.ru>
2023-11-06 12:17:57 +0500

    Create input,  ref appJs

--- src/js/app.js
+++ src/js/app.js
@@ -4,57 +4,100 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
+// Убрать
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
 
-let city = "";
-// London, Moscow, Berlin, Frankfurt, Shanghai, Beijing
-document.getElementById("button1").onclick = function() {
-    city = 'London';
+
+
+
+// Сделать делегирование на кнопки
+// let city = "";
+// London, Moscow, Berlin, Shanghai, Beijing
+// document.getElementById("button1").onclick = function() {
+//     city = 'London';
     
-};
+// };
 
-document.getElementById("button2").onclick = function() {
-    city = 'Moscow';
+// document.getElementById("button2").onclick = function() {
+//     city = 'Moscow';
     
-};
+// };
 
-document.getElementById("button3").onclick = function() {
-    city = 'Berlin';
+// document.getElementById("button3").onclick = function() {
+//     city = 'Berlin';
     
-};
+// };
 
-document.getElementById("button4").onclick = function() {
-    city = 'Frankfurt';
+// document.getElementById("button4").onclick = function() {
+//     city = 'Frankfurt';
     
-};
+// };
 
-document.getElementById("button5").onclick = function() {
-    city = 'Shanghai';
+// document.getElementById("button5").onclick = function() {
+//     city = 'Shanghai';
 
     
-};
+// };
 
-const buttons = document.querySelectorAll('.button');
-      buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=96ecd3f33dcc527de032893cda86702c`)
+// const buttons = document.querySelectorAll('.button');
+//       buttons.forEach(button => {
+//         button.addEventListener('click', () => {
+//             fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=96ecd3f33dcc527de032893cda86702c`)
+//             .then(function (resp){ return resp.json() })
+//             .then(function (data) {
+//                 console.log(data);
+//                 document.querySelector('.wind').innerHTML = data.wind.speed;
+//                 document.querySelector('.temperature').innerHTML = Math.round(data.main.temp - 273);
+//             })
+//             .catch(function () {
+                
+//             });
+//         });
+//       });
+
+
+      const buttonContainer = document.getElementById('button-container');
+
+      buttonContainer.addEventListener('click', function(event) {
+        if (event.target.className === 'button') {
+          // Вы можете выполнить здесь код, связанный с кнопкой, которая была нажата
+          fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.textContent}&appid=96ecd3f33dcc527de032893cda86702c`)
             .then(function (resp){ return resp.json() })
             .then(function (data) {
                 console.log(data);
-                document.querySelector('.wind').innerHTML = data.wind.speed;
-                document.querySelector('.temperature').innerHTML = Math.round(data.main.temp - 273);
+                document.querySelector('.wind').innerHTML = data.wind.speed + " М/с";
+                document.querySelector('.temperature').innerHTML = Math.round(data.main.temp - 273) + "&deg" ;
             })
             .catch(function () {
-                console.error("Ошибка");
+                
             });
-        });
+        }
       });
 
 
-      
+      let inputVal;
+      document.querySelector(".input-button").onclick = function(){
+        inputVal = document.querySelector("input").value;
+        console.log(inputVal);
+        document.querySelector('.text-information').innerHTML = inputVal;
 
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=96ecd3f33dcc527de032893cda86702c`)
+            .then(function (resp){ return resp.json() })
+            .then(function (data) {
+                console.log(data);
+                document.querySelector('.wind').innerHTML = data.wind.speed + " М/с";
+                document.querySelector('.temperature').innerHTML = Math.round(data.main.temp - 273) + "&deg" ;
+            })
+            .catch(function () {
+                
+            });
+            document.querySelector("input").value = "";
+
+      }
+
+      
 // fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=96ecd3f33dcc527de032893cda86702c`)
 //     .then(function (resp){ return resp.json() })
 //     .then(function (data) {
@@ -66,6 +109,8 @@ const buttons = document.querySelectorAll('.button');
 
 //     });
 
+
+//   Cors запрещает доступ к ресурсам сайта
 
 
 
